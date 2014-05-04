76072f44cc8818858b70c9daf2ad75fa79044fa3
Marko Grešak <gresak.marko@gmail.com>
2014-05-04 03:01:17 +0200

    Popravljeni bug v izdaji, manjsi bugi in odgovori

--- LDN7/streznik/Skladisce.java
+++ LDN7/streznik/Skladisce.java
@@ -1,157 +1,176 @@
 package streznik;
 
-import java.util.ArrayList;
-
 import transakcija.Artikel;
 import transakcija.TipTransakcije;
 import transakcija.Transakcija;
 
-public class Skladisce {
+import java.util.ArrayList;
 
-	private static ArrayList<Skladisce> skladisca = new ArrayList<Skladisce>();
+class Skladisce {
 
-	private int _idSkladisca;
-	private ArrayList<String> _stranke;
-	private ArrayList<ArrayList<Artikel>> _artikli;
+    private static final ArrayList<Skladisce> skladisca = new ArrayList<Skladisce>();
 
-	public Skladisce(int idSkladisca) {
-		this._idSkladisca = idSkladisca;
-	}
+    private final int _idSkladisca;
+    private final ArrayList<String> _stranke;
+    private final ArrayList<ArrayList<Artikel>> _artikli;
 
-	public int getIdSkladisca() {
-		return _idSkladisca;
-	}
+    private Skladisce(int idSkladisca) {
+        this._idSkladisca = idSkladisca;
+        this._stranke = new ArrayList<String>();
+        this._artikli = new ArrayList<ArrayList<Artikel>>();
+    }
 
-	public ArrayList<String> getStranke() {
-		return _stranke;
-	}
+    private static Transakcija dobava(Transakcija t) {
+        Artikel[] art = t.getArtikli();
+        for (Artikel anArt : art) najdiSkladisce(anArt).spremeniArtikel(t.getStranka(), anArt, true);
+        return t.generateOdgovor();
+    }
 
-	public ArrayList<ArrayList<Artikel>> getArtikli() {
-		return _artikli;
-	}
+    private static Transakcija izdaja(Transakcija t) {
+        Artikel[] art = t.getArtikli();
+        for (Artikel anArt : art) {
+            Skladisce s = najdiSkladisce(anArt);
+            if (!s.spremeniArtikel(t.getStranka(), anArt, false)) {
+                int iStranka = s.najdiStranka(t.getStranka(), false);
+                if (iStranka < 0)
+                    return t.setOdgovor(String.format("Napaka pri izdaji artikla %s, stranka nima zalog v tem skladiscu!", anArt.getNaziv()));
+                else {
+                    int iArtikel = s.najdiArtikel(anArt, iStranka);
+                    if (iArtikel < 0)
+                        return t.setOdgovor(String.format("Napaka pri izdaji artikla %s, artikel ne obstaja v skladiscu stranke!", anArt.getNaziv()));
+                    else
+                        return t.setOdgovor(String.format("Napaka pri izdaji artikla %s, zahtevana kolicina = %d, stanje v skladiscu = %d",
+                                anArt.getNaziv(),
+                                anArt.getKolicina(),
+                                s.getArtikliStranka(iStranka).get(iArtikel).getKolicina()));
+                }
+            }
+        }
+        return t.generateOdgovor();
+    }
 
-	private ArrayList<Artikel> getArtikliStranka(int iStranka) {
-		if (iStranka < 0)
-			return null;
-		return _artikli.get(iStranka);
-	}
+    private static Transakcija inventura(Transakcija t) {
+        String stranka = t.getStranka();
+        ArrayList<Artikel> art = new ArrayList<Artikel>();
+        ArrayList<Skladisce> skl = najdiSkladiscaStranka(stranka);
+        for (Skladisce s : skl) {
+            int iStranka = s.najdiStranka(stranka, false);
+            if (iStranka == -1)
+                continue;
+            art.addAll(s.getArtikliStranka(iStranka));
+        }
 
-	private int najdiStranka(String stranka) {
-		int index = -1;
-		if (_stranke.contains(stranka))
-			for (int i = 0; i < _stranke.size(); i++)
-				if (stranka.equals(_stranke.get(i))) {
-					index = i;
-					break;
-				}
-		if (index == -1) {
-			index = _stranke.size();
-			_stranke.add(stranka);
-		}
-		return index;
-	}
+        if (art.size() == 0)
+            return new Transakcija(TipTransakcije.Inventura, stranka, new Artikel[]{Artikel.empty()},
+                    String.format("Stranka %s nima v skladiscih nobenega artikla.", stranka));
+        else
+            return new Transakcija(TipTransakcije.Inventura, stranka, art.toArray(new Artikel[art.size()])).generateOdgovor();
+    }
 
-	private int najdiArtikel(Artikel artikel, int iStranka) {
-		int index = -1;
-		ArrayList<Artikel> artstranka = getArtikliStranka(iStranka);
-		if (artstranka.contains(artikel))
-			for (int i = 0; i < artstranka.size(); i++)
-				if (artikel.equals(artstranka.get(i))) {
-					index = i;
-					break;
-				}
-		if (index == -1) {
-			index = artstranka.size();
-			_artikli.get(iStranka).add(artikel);
-		}
-		return index;
-	}
+    public static Transakcija obdelajTransakcijo(Transakcija t) {
+        if (t.getTip().equals(TipTransakcije.Dobava))
+            return dobava(t);
+        else if (t.getTip().equals(TipTransakcije.Izdaja))
+            return izdaja(t);
+        else if (t.getTip().equals(TipTransakcije.Inventura))
+            return inventura(t);
+        else
+            return null;
+    }
 
-	private boolean spremeniArtikel(String stranka, Artikel artikel,
-			boolean dobava) {
-		int iStranka = najdiStranka(stranka);
-		int iArtikel = najdiArtikel(artikel, iStranka);
-		if (iArtikel < getArtikliStranka(iStranka).size() - 1)
-			return _artikli
-					.get(iStranka)
-					.get(iArtikel)
-					.spremeniKolicino(artikel.getKolicina() * (dobava ? 1 : -1));
-		return true;
-	}
+    private static Skladisce najdiSkladisce(Artikel a) {
+        int index = -1;
+        for (int i = 0; i < skladisca.size(); i++)
+            if (skladisca.get(i).getIdSkladisca() == a.getSkladisce()) {
+                index = i;
+                break;
+            }
+        if (index == -1) {
+            skladisca.add(new Skladisce(a.getSkladisce()));
+            index = skladisca.size() - 1;
+        }
+        return skladisca.get(index);
+    }
 
-	private static Transakcija dobava(Transakcija t) {
-		Artikel[] art = t.getArtikli();
-		for (int i = 0; i < art.length; i++)
-			najdiSkladisce(art[i])
-					.spremeniArtikel(t.getStranka(), art[i], true);
-		return t.generateOdgovor();
-	}
+    private static ArrayList<Skladisce> najdiSkladiscaStranka(String stranka) {
+        ArrayList<Skladisce> skl = new ArrayList<Skladisce>();
+        for (Skladisce s : skladisca)
+            for (String str : s.getStranke())
+                if (str.equals(stranka)) {
+                    skl.add(s);
+                    break;
+                }
+        return skl;
+    }
 
-	private static Transakcija izdaja(Transakcija t) {
-		Artikel[] art = t.getArtikli();
-		for (int i = 0; i < art.length; i++) {
-			Skladisce s = najdiSkladisce(art[i]);
-			if (!s.spremeniArtikel(t.getStranka(), art[i], true))
-				return t.setOdgovor(String
-						.format("Napaka pri izdaji artikla %s, zahtevana kolicina = %d, stanje v skladiscu = %d",
-								art[i].getNaziv(),
-								art[i].getKolicina(),
-								s.getArtikliStranka(
-										s.najdiStranka(t.getStranka()))
-										.get(s.najdiArtikel(art[i],
-												s.najdiStranka(t.getStranka())))
-										.getKolicina()));
-		}
-		return t.generateOdgovor();
-	}
+    int getIdSkladisca() {
+        return _idSkladisca;
+    }
 
-	private static Transakcija stanjeSkladisca(String stranka) {
-		ArrayList<Artikel> art = new ArrayList<Artikel>();
-		ArrayList<Skladisce> skl = najdiSkladiscaStranka(stranka);
-		for (Skladisce s : skl)
-			art.addAll(s.getArtikliStranka(s.najdiStranka(stranka)));
-		return new Transakcija(TipTransakcije.Inventura, stranka,
-				art.toArray(new Artikel[art.size()])).generateOdgovor();
-	}
+    ArrayList<String> getStranke() {
+        return _stranke;
+    }
 
-	public static Transakcija obdelajTransakcijo(Transakcija t) {
-		if (t.getTip().equals(TipTransakcije.Dobava))
-			return dobava(t);
-		else if (t.getTip().equals(TipTransakcije.Izdaja))
-			return izdaja(t);
-		else if (t.getTip().equals(TipTransakcije.Inventura))
-			return stanjeSkladisca(t.getStranka());
-		else
-			return null;
-	}
+//    public ArrayList<ArrayList<Artikel>> getArtikli() {
+//        return _artikli;
+//    }
 
-	@Override
-	public boolean equals(Object obj) {
-		return ((Skladisce) obj)._idSkladisca == this._idSkladisca;
-	}
+    private ArrayList<Artikel> getArtikliStranka(int iStranka) {
+        if (iStranka < 0)
+            return null;
+        return _artikli.get(iStranka);
+    }
 
-	public static Skladisce najdiSkladisce(Artikel a) {
-		int index = -1;
-		for (int i = 0; i < skladisca.size(); i++)
-			if (skladisca.get(i).getIdSkladisca() == a.getSkladisce()) {
-				index = i;
-				break;
-			}
-		if (index == -1) {
-			skladisca.add(new Skladisce(a.getSkladisce()));
-			index = skladisca.size() - 1;
-		}
-		return skladisca.get(index);
-	}
+    private int najdiStranka(String stranka, boolean dodajNovo) {
+        int index = -1;
+        if (_stranke.size() > 0 && _stranke.contains(stranka))
+            for (int i = 0; i < _stranke.size(); i++)
+                if (stranka.equals(_stranke.get(i))) {
+                    index = i;
+                    break;
+                }
+        if (index == -1 && dodajNovo) {
+            index = _stranke.size();
+            _stranke.add(stranka);
+            _artikli.add(new ArrayList<Artikel>());
+        }
+        return index;
+    }
 
-	public static ArrayList<Skladisce> najdiSkladiscaStranka(String stranka) {
-		ArrayList<Skladisce> skl = new ArrayList<>();
-		for (Skladisce s : skladisca)
-			for (String str : s.getStranke())
-				if (str.equals(stranka)) {
-					skl.add(s);
-					break;
-				}
-		return skl;
-	}
+    private int najdiArtikel(Artikel artikel, int iStranka) {
+        int index = -1;
+        ArrayList<Artikel> artStranka = getArtikliStranka(iStranka);
+        if (artStranka.contains(artikel))
+            for (int i = 0; i < artStranka.size(); i++)
+                if (artikel.equals(artStranka.get(i))) {
+                    index = i;
+                    break;
+                }
+
+        return index;
+    }
+
+    private boolean spremeniArtikel(String stranka, Artikel artikel, boolean dobava) {
+        int iStranka = najdiStranka(stranka, dobava);
+        if (iStranka == -1)
+            return false;
+        int iArtikel = najdiArtikel(artikel, iStranka);
+        if (iArtikel == -1) {
+            if (dobava) {
+//                iArtikel = _artikli.get(iStranka).size();
+                _artikli.get(iStranka).add(artikel);
+                return true;
+            } else
+                return false;
+        }
+        boolean rezultat = _artikli.get(iStranka).get(iArtikel).spremeniKolicino(artikel.getKolicina() * (dobava ? 1 : -1));
+        if (_artikli.get(iStranka).get(iArtikel).getKolicina() == 0)
+            _artikli.get(iStranka).remove(iArtikel);
+        return rezultat;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof Skladisce) && ((Skladisce) obj)._idSkladisca == this._idSkladisca;
+    }
 }
